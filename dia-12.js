416e14d8c31a6ca51014a2681af2fc7bac756154
Roberto Chambi <rcchambi4@umsa.bo>
2023-01-12 20:19:48 -0400

    Dia 12 agregado

---
+++ dia-12.js
@@ -0,0 +1,18 @@
+function dia12(distancia, trineos){
+    let vector = []
+    trineos.forEach((elemento, index) =>{
+        vector[index] = elemento.consumption*distancia
+    })
+    let trineo = null
+    for(let i = 0; i < vector.length; i++){
+        if(vector[i] <= 20){
+            trineo = trineos[i].name
+        }
+    }
+    console.log(trineo)
+}
+dia12(10, [
+    { name: 'Pedrosillano', consumption: 1 },
+    { name: 'SamarJaffal', consumption: 2 },
+    { name: 'marcospage', consumption: 3 }
+  ])
